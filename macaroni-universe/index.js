2aa125d244b6ddafcd7ab03f12b436c5d7ff9b77
Paughton <113547668+SilasCarlson@users.noreply.github.com>
2025-12-09 22:57:54 -0700

    Progress on final project

--- macaroni-universe/index.js
+++ macaroni-universe/index.js
@@ -1,9 +1,39 @@
 import recipes from "./recipes.mjs";
 
+function getCategoriesTemplate(categories) {
+    let template = ``;
+    categories.forEach((category, index) => {
+        if (index > 0) template += ", ";
+        template += `${category}`;
+    });
+
+    return template;
+}
+
 function getFeaturedRecipeTemplate(recipe) {
-    return ``;
+    return `
+        <article class="recipe-preview">
+            <img src="${recipe.image}" alt="${recipe.name}">
+            <div class="recipe-preview-content">
+                <p>${getCategoriesTemplate(recipe.categories)}</p>
+                <h3>${recipe.name}</h3>
+                <p>${recipe.review}</p>
+                <a href="recipes.html?id=${recipe.id}">Read more</a>
+            </div>
+        </article>
+    `;
 }
 
 // Select 3 random recipes
+const randomRecipes = [ recipes[0], recipes[1], recipes[2], recipes[2] ];
 
 // Add the featured recipes
+const recipeContainer = document.querySelector(".recipe-preview-container");
+randomRecipes.forEach((recipe) => {
+    recipeContainer.innerHTML += getFeaturedRecipeTemplate(recipe);
+});
+
+// Event Listeners
+document.getElementById("#our-mission-call-to-action").addEventListener("click", () => {
+    window.location.href = "recipes.html";
+});
--- macaroni-universe/recipes.mjs
+++ macaroni-universe/recipes.mjs
@@ -2,6 +2,10 @@ const recipes = [
     {
         id: 1,
         name: "Gluten-Free Mozzarella Mac and Cheese",
+        categories: [
+            "Healthy",
+            "Gluten-free"
+        ],
         ingredients: [
             "12 oz gluten-free macaroni noodles - 340g",
             "3 tablespoons unsalted butter - 43g",
@@ -28,9 +32,87 @@ const recipes = [
             "Top with fresh basil if desired. ",
             "Serve immediately.",
         ],
-        review_stars: 3,
+        rating: 3,
         review: "This macaroni and cheese is very tasty and is perfect for anyone who is not able to eat gluten, but still wants to partake in the delicousness of Macaroni and Cheese.",
         image: "images/gluten-free-mozzarella-mac-and-cheese.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Instant Pot Mac and Cheese",
+        categories: [
+            "Fast"
+        ],
+        ingredients: [
+            "1 pound (454g) elbow macaroni",
+            "4 cups (1L) cold water",
+            "4 tablespoons (60g) unsalted butter",
+            "14 ounces (397g) sharp cheddar , freshly grated",
+            "6 ounces (170g) mild cheddar or Velveeta cheese , freshly grated",
+            "Kosher salt and ground black pepper",
+            "2 large eggs , beaten",
+            "1 teaspoon ground mustard",
+            "12 ounces (355ml can) evaporated milk",
+            "Optional: 1 teaspoon Sriracha sauce or Frank's hot sauce"
+        ],
+        directions: [
+            "Pressure Cook Macaroni: Add 1lb (454g) elbow macaroni, 4 cups (1L) cold water, and a pinch of kosher salt in Instant Pot.",
+            "Close the lid, then turn Venting Knob to Sealing Position. Pressure cook at High Pressure for 4 minutes, then Gradual Quick Release. Carefully open the lid.",
+            "Mix Wet Ingredients: While the macaroni is pressure cooking, mix the wet ingredients in a medium mixing bowl. Beat 2 large eggs, then mix in 1 tsp ground mustard, 1 tsp Sriracha, and 12 ounces (355ml) evaporated milk. Mix well.",
+            "Make Mac & Cheese: Use \"Keep Warm\" function. Give it a quick stir. If there's more than ¾ cup of water left in the pot, drain some of the liquid.",
+            "Add 4 tbsp unsalted butter to the pressure cooked macaroni. Mix well with a silicone spatula and let the butter melt. Pour in the wet ingredients and mix well.",
+            "Add grated cheese (⅓ portion at a time), then stir constantly until the cheese fully melt.",
+            "Serve: Taste and season with kosher salt and ground black pepper. You'll likely need a couple pinches of kosher salt to brighten the dish."
+        ],
+        rating: 4,
+        review: "The most classic examples of Macaroni and Cheese, this splendid concoction will bring your taste buds onto a holy pilgrimage.",
+        image: "images/instant-pot-mac-and-cheese.jpg"
+    },
+
+    {
+        id: 3,
+        name: "Baked Macaroni and Cheese",
+        categories: [
+            "Baked",
+            "Oven",
+            "Many Cheese!"
+        ],
+        ingredients: [
+            "250g / 8 oz macaroni (elbow pasta)",
+            "1 tbsp (15g) unsalted butter (or 2 tsp oil)",
+            "2/3 cup panko breadcrumbs (Note 1)",
+            "2 tbsp (30g) unsalted butter , melted",
+            "1/4 tsp salt",
+            "4 tbsp (60g) unsalted butter",
+            "1/3 cup flour , plain / all purpose",
+            "3 cups milk , warmed (low or full fat)",
+            "2 cups freshly shredded cheese, gruyere best (followed by cheddar and Colby)",
+            "1 cup freshly shredded mozzarella cheese",
+            "3/4 tsp salt",
+            "1 tsp garlic powder",
+            "1/2 tsp onion powder",
+            "1/2 tsp mustard powder"
+        ],
+        directions: [
+            "Cook pasta: Bring a large pot of water to the boil. Add macaroni and cook per packet directions MINUS 1 minute.",
+            "Toss in butter: Drain, return pasta to pot, add butter and toss until melted." ,
+            "Set aside to cool while making the Sauce",
+            "Mix together Topping. Set aside.",
+            "Preheat oven to 180°C/350°F (all oven types).",
+            "Make roux: In a large saucepan or in an ovenproof skillet (I use my 26cm/9 Lodge cast iron skillet), melt butter over medium heat. Add flour and cook, stirring constantly, for 1 minute.",
+            "Add milk: Add about 1 cup of the milk and mix to dissolve the paste into the milk.",
+            "Then add remaining milk and mix until lump free (use whisk if required).",
+            "Add Seasonings: Mix in salt and Seasonings if using.",
+            "Thicken sauce: Cook, stirring/whisking regularly, for 5 - 8 minutes until thickened to a cream consistency. When the Sauce coats the back of a wooden spoon, you should be able to draw a path with your finger.",
+            "Add cheese: Remove from stove, add cheese and stir - cheese doesn't need to melt.",
+            "Check salt: Adjust salt to taste (if you use recommended cheeses, you won't need more).",
+            "Assemble: Pour Sauce into pot with Macaroni. Stir quickly, then pour back into the skillet (I did this) or a baking dish (Note 4). Sprinkle with breadcrumb topping.",
+            "Bake for 25 minutes or until top is light golden. Don't bake too long otherwise you'll bake away the Sauce!",
+            "Serve: Serve immediately! I sprinkled mine with a bit of fresh parsley.",
+        ],
+        rating: 5,
+        review: "Who doesn't love baked macaroni and cheese? This recipe makes arguably one of the best macaroni and cheese's divine. I love mac and cheese, but baked mac and cheese really just activates my tastbuds.",
+        image: "images/baked-macaroni-and-cheese.webp"
     }
 ]
 
